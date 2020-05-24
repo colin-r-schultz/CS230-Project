fa82b5acd61df78651d6dae179bbc087adf5f693
Colin Schultz <colinschultz@colins-mbp-2.attlocal.net>
2020-05-24 12:49:14 -0700

    fixed embedding size

--- architectures/recurrent_fc.py
+++ architectures/recurrent_fc.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
+import architectures.recurrent_deconv as recurrent_deconv
 from architectures.recurrent_deconv import representation_network
 from constants import *
 
-NUM_INPUT_OBS = 28
+NUM_INPUT_OBS = 24
 NUM_TEST_OBS = 1 # doesn't use test obs (no localization)
 
-EMBEDDING_SIZE = 256
+EMBEDDING_SIZE = 512
+recurrent_deconv.EMBEDDING_SIZE = EMBEDDING_SIZE
 
 CHECKPOINT_PATH = 'recurrent_fc_v2'
 
